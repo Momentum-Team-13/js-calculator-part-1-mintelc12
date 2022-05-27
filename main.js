593f4339dde510bb3afe642eeac5614593cac26c
Chloe Mintel <mintelc12@gmail.com>
2022-05-27 15:47:19 -0400

    finally figured out how to make my calculator work by using if/else statements. will begin working on bonus tasks now.

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
 console.log("connected")
-let numbers =
-document.querySelectorAll(".number");
-console.log(numbers);
+let numbers = document.querySelectorAll(".number");
+let equals = document.getElementById("evaluation");
+let display = document.querySelector("#display")
+let clear = document.querySelector(".clear")
+// console.log(number);
 
-let display = document.querySelector("#blank")
 
 for (let number of numbers) {
     number.addEventListener("click",
     function(event) {
+    console.log(number.innerText)
+    
+    if (number === equals) {
+        display.innerText = math.eval(display.innerText);
 
-        console.log("event object: ", "event")
-        console.log(event.target.textContent);
-        let newElement = 
-    document.createElement("p");
-    // create a new element, in this case a p
-        newElement.innerText = `${event.target.textContent}`
-        // updated the inner text of that new element to be the contents of the clicked box
-        display.appendChild(newElement);
+    } else {
+        display.innerText += event.target.textContent;
+    }
+    // console.log("event object: ", "event")
+    //     console.log(event.target.textContent);
+    //     let newElement = 
+    // document.createElement("p");
+    // // create a new element, in this case a p
+    //     newElement.innerText = `${event.target.textContent}`
+    //     // updated the inner text of that new element to be the contents of the clicked box
+    //     display.appendChild(newElement);
         // append the new element containing the p tag
         // to the currently empty display
     });
 }
-let clear = document.querySelector(".clear")
+
+
 // for (let clear of clear) {
     clear.addEventListener("click", 
     function(event) {
         display.innerText = "";
-        // console.log("")
-        // console.log(event.target.textContent);
     })
 
+
+    
