8f469866123e41c0bc06d0af22d134548d1ffba3
Chloe Mintel <mintelc12@gmail.com>
2022-05-29 09:56:19 -0400

    allowed keyboard input to function. now I will need to figure out the clear sign.

--- main.js
+++ main.js
@@ -23,12 +23,48 @@ for (let number of numbers) {
     });
 }
 
+document.addEventListener("keydown", (event) => {
+    if (event.key == "0") {
+        display.innerText += "0";
+    } else if (event.key == "1") {
+        display.innerText += "1";
+    } else if (event.key == "2") {
+        display.innerText += "2"; 
+    } else if (event.key == "3") {
+        display.innerText += "3";
+    } else if (event.key == "4") {
+        display.innerText += "4";
+    } else if (event.key == "5") {
+        display.innerText += "5";
+    } else if (event.key == "6") {
+        display.innerText += "6";
+    } else if (event.key == "7") {
+        display.innerText += "7";
+    } else if (event.key == "8") {
+        display.innerText += "8";
+    } else if (event.key == "9") {
+        display.innerText += "9";
+    } else if (event.key == "0") {
+        display.innerText += "0";
+    } else if (event.key == "/") {
+        display.innerText += "/";
+    } else if (event.key == "+") {
+        display.innerText += "+";
+    } else if (event.key == "=") {
+        display.innerText = math.eval(display.innerText);    
+    } else if (event.key == "*") {
+        display.innerText += "*";
+    } else if (event.key == "-") {
+        display.innerText += "-";
+    }
+    } 
+)
 
 // for (let number of numbers) {
-//     number.addEventListener("keyup", function(event) { 
-//         console.log(number.innerText)
-//         display.innerText += event.target.textContent;
-//         })};
+//     number.addEventListener("keypress", function(event) { 
+
+//         return display.innerText += event.target.textContent;
+//     })};
 
 
         clear.addEventListener("click", 
